f3fc68b45a39bbd4c30e5667b1e54712ea94b2c4
saleszera <crowzcoleslaw@gmail.com>
2022-01-13 18:12:14 -0300

    add current user geoposition on map

--- src/screens/Main/index.tsx
+++ src/screens/Main/index.tsx
@@ -1,28 +1,65 @@
+import { useEffect, useState } from 'react';
 import MapView, { Marker, Callout } from 'react-native-maps';
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, Alert } from 'react-native'
 import { useNavigation } from '@react-navigation/native'
 import { NativeStackNavigationProp } from '@react-navigation/native-stack'
+import * as Location from 'expo-location';
 
 import { locations } from '../../utils/institutions'
 import { RootStackParamList } from '../../../types'
 import { styles } from './styles'
 
 type MainScreenProp = NativeStackNavigationProp<RootStackParamList, 'Main'>
+type ICurrentLocation = {
+  latitude: number,
+  longitude: number,
+  latitudeDelta: number,
+  longitudeDelta: number,
+}
 
 const Main: React.FC = () => {
-  const initialRegion = {
-    latitude: -22.7309943,
-    longitude: -45.1464314,
-    latitudeDelta: 0,
-    longitudeDelta: 0.09,
-  }
+  const [currentLocation, setCurrentLocation] = useState<ICurrentLocation>({} as ICurrentLocation)
+  const [loading, setLoading] = useState(true)
 
   const { navigate } = useNavigation<MainScreenProp>()
 
+  useEffect(() => {
+    (async () => {
+      const { status } = await Location.requestForegroundPermissionsAsync();
+
+      if(status !== 'granted'){
+        console.log('Permission is not granted')
+        return;
+      }
+
+      const currentPosition = await Location.getCurrentPositionAsync({accuracy: 5})
+
+      if(currentPosition){
+        setCurrentLocation(
+          {
+            latitude: currentPosition.coords.latitude,
+            longitude: currentPosition.coords.longitude,
+            latitudeDelta: 0.5,
+            longitudeDelta: 0.11,
+          }
+        )
+      }
+
+      setLoading(false)
+    })();
+  }, [])
+
+  if(loading){
+    return (
+      <View style={styles.loadingContainer}>
+        <Text style={styles.loadingText}>Carregando...</Text>
+      </View>
+    )
+  }
 
   return (
     <MapView
-      initialRegion={initialRegion}
+      initialRegion={currentLocation}
       style={{flex: 1}}
     >
       {locations.map((item) => (
